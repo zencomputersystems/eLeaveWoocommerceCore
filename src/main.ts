d7abe71c55a82b14ce11ff06c76ce48572eeb857
Fakhri <fakhrialuwe@rocketmail.com>
2020-07-24 14:27:04 +0800

    update cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   const options = new DocumentBuilder()
     .setTitle('eLeave Subscription')
